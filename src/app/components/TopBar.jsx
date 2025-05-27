cd9de56e1391999b8630ff6c535dcf3f38b12864
DevGlauber <devglaubermonteiro@gmail.com>
2025-05-27 20:34:11 -0300

    ajustado title leader

--- src/app/components/TopBar.jsx
+++ src/app/components/TopBar.jsx
@@ -2,7 +2,7 @@ import { Menu } from "lucide-react";
 
 export default function Presentation() {
   return (
-    <div className="fixed flex px-8 py-6 bg-black/50 w-full  justify-between text-white z-50">
+    <div className="fixed flex p-6 bg-black/40 w-full  justify-between text-white z-50 backdrop-blur-sm">
       <span className="font-bold">Bereshit</span>
       <Menu />
     </div>
