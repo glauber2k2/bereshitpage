6b0f6515bf1ee0f80d9d686f917025b57f739ea9
Glauber Monteiro <103267667+glauber2k2@users.noreply.github.com>
2025-06-02 16:10:48 -0300

    Atualizar o quizzes.jsx

--- src/data/quizzes.jsx
+++ src/data/quizzes.jsx
@@ -27,4 +27,128 @@ export const quizzes = [
       },
     ],
   },
+
+  {
+  "id": "genesisum",
+  "title": "Quiz de Gênesis - Capítulo 1",
+  "questions": [
+    {
+      "question": "O que ou quem se movia sobre as águas enquanto a Terra era sem forma, vazia e havia trevas sobre a face das águas?",
+      "options": ["Jesus", "A escuridão", "O espírito", "Uma névoa"],
+      "correctAnswer": 2
+    },
+    {
+      "question": "o que Deus fez no segundo dia ?",
+      "options": ["O firmamento e fez separação de águas e águas.", "Criou o sol, a lua e estrelas.", "O firmamento e os seres que estejam.", "A vegetação ."],
+      "correctAnswer": 0
+    },
+    {
+      "question": "No terceiro dia , após Deus ter ordenado que a porção seca ( chamada terra) , depois de nomear o ajuntamento de águas de marés, o que mais ele ordenou?",
+      "options": ["Que os mares se movessem sobre a terra.", "Que a Terra produzisse relva, e as ervam dessem sementes e as árvores dessem frutos.", "Que as aves fossem criadas, e os anfíbios e répteis também.", "Nenhuma das opções."],
+      "correctAnswer": 1
+    },
+    {
+      "question": "Quem foram os animais que Deus abençoou na criação?",
+      "options": ["Répteis e gafanhotos.", "Aves , animais marinhos e répteis aquáticos.", "Aves e peixes .", "Deus não abençoou animais."],
+      "correctAnswer": 1
+    },
+    {
+      "question": "Quantas vezes em Gênesis 1 está escrito: “ E viu Deus que era bom”?",
+      "options": ["3", "6", "7", "Nenhuma vez."],
+      "correctAnswer": 1
+    },
+    {
+      "question": "Sobre o quarto dia da criação responda corretamente, para que servem os luzeiros ou liminares criados por Deus:",
+      "options": ["Governar dia e noite, e para servirem de proteção.", "Para direcionar os astros e horóscupo.", "Para governar dia e noite, e servirem de sinais, estações,  dias e anos.", "Para a lua ser cheia de luz por conta do sol que é um astro e não apenas uma estrela."],
+      "correctAnswer": 2
+    },
+    {
+      "question": "No sexto dia da criação Deus ordenou que quem produzisse animais que fossem répteis, domésticos e selvagens.",
+      "options": ["O vento", "Ele mesmo", "A Terra", "O espírito"],
+      "correctAnswer": 2
+    },
+    {
+      "question": "Em que dia da criação Deus ordenou que as aves voassem abaixo e acima do firmamento dos céus?",
+      "options": ["No segundo", "No primeiro", "No quinto", "No último"],
+      "correctAnswer": 2
+    },
+    {
+      "question": "Ao final do capítulo 1 relata que Deus ordena algo ao homem e a mulher, o que ele disse :",
+      "options": ["Sejam de fundos , se multipliquem e dominem a terra e tudo que nela há, assim como todos os animais.", "Sejam expulsos do jardim", "Dominem apenas os animais que são terrestres.", "Comam de tudo , menos da árvore do conhecimento do bem e do mal."],
+      "correctAnswer": 0
+    },
+    {
+      "question": "O que Deus deu a Adão e Eva como mantimento?",
+      "options": ["trigo", "ervas verdes", "Frutas", "Maçãs"],
+      "correctAnswer": 1
+      }
+    ]
+  },
+
+  {
+  "id": "genesis2",
+  "title": "Quiz de Gênesis - Capítulo 2",
+  "questions": [
+    {
+      "question": "Por quais motivos o capítulo 2 afirma que ainda não havia brotado erva na terra ?",
+      "options": [
+        "Porque a terras estava infértil e seca.",
+        "Porque ainda não havia chovido e o homem não tinha sido criado para lavrar a terra.",
+        "Porque Adão não teria como lavrar a terra dedido o cuidado com os animais .",
+        "Porque não era tempo de colheita."
+      ],
+      "correctAnswer": 1
+    },
+    {
+      "question": "No jardim no Éden tinha um rio que se divida em 4 braços, como eles se chamavam?",
+      "options": [
+        "Nilo, Mesopotâmia, Eufrates Havilá",
+        "Pisom, Nilo, Giom, Eufrates",
+        "Pisom, Giom, Tigre e Eufrates.",
+        "Tigre , Cuxe, Pisom e Nilo."
+      ],
+      "correctAnswer": 2
+    },
+    {
+      "question": "No capítulo 2 Deus fala quais era as funções do homem no jardim, e eram :",
+      "options": [
+        "Colocar ordem no jardim e regar .",
+        "Cultivar e guardar",
+        "Cuidar das árvores e dos rios.",
+        "Para alimentar os animais e peixes ."
+      ],
+      "correctAnswer": 1
+    },
+    {
+      "question": "O que era produzido em Havilá?",
+      "options": [
+        "Ouro e Bdélio",
+        "Prata e marfim",
+        "Ônix",
+        "Joias e tesouros"
+      ],
+      "correctAnswer": 0
+    },
+    {
+      "question": "Após criar os animais serem criados o que Deus fez ?",
+      "options": [
+        "Os nomeou.",
+        "Levou eles para serem nomeados por Adão.",
+        "Os ordenou a não falarem .",
+        "Deu a cada um uma forma de se comunicar."
+      ],
+      "correctAnswer": 1
+    },
+    {
+      "question": "Entre os versículos 15 e 21 Deus expressa uma ordem muito Clara para Adão que foi:",
+      "options": [
+        "Te farei uma auxiliadora, e ela será sua mulher.",
+        "Não comando fruto da árvore da vida .",
+        "Não coma do fruto do conhecimento do bem e do mal.",
+        "Durma que farei para ti uma mulher."
+      ],
+      "correctAnswer": 2
+      }
+    ]
+  },
 ];
